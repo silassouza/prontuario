ee83c27e9df6c7d79fd2f7c4d05463600851d7aa
Silas de Souza Silva <ssilva@localhost.localdomain>
2018-01-14 23:51:53 -0300

    ...

--- app.js
+++ app.js
@@ -63,8 +63,9 @@ app.use('/cadastro', cadastro)
 app.use('/consultas', consultas)
 
 // Set Port
-app.set('port', (process.env.PORT || 3000))
-
-app.listen(app.get('port'), function(){
-	console.log('Server started on port '+app.get('port'))
-})
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+app.listen(port, address, function () {
+  console.log( "Listening on " + address + ", port " + port )
+});
--- models/db.js
+++ models/db.js
@@ -1,7 +1,13 @@
 var mongoose = require('mongoose');
 
+var db_name = 'prontuario'
+var mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
+if(process.env.OPENSHIFT_MONGODB_DB_URL){
+  mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
+}
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/prontuario');
+mongoose.connect(mongodb_connection_string);
 
 mongoose.connection.on('connected', function () {
   console.log('Mongoose default connection open');
