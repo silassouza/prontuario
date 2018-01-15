dd0408fbfab2f88fec53a74322525ca842dd2cdb
Silas <silasq@gmail.com>
2018-01-15 18:35:44 -0300

    ...

--- app.js
+++ app.js
@@ -69,3 +69,5 @@ var address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 app.listen(port, address, function () {
   console.log( "Listening on " + address + ", port " + port )
 });
+
+module.exports = app;
