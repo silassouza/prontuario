54bfebaa326559c054fc6ee3674d14b067b4dfaa
Silas <silasq@gmail.com>
2018-01-15 19:01:00 -0300

    ...

--- models/db.js
+++ models/db.js
@@ -1,13 +1,29 @@
 var mongoose = require('mongoose');
 
-var db_name = 'prontuario'
-var mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
-if(process.env.OPENSHIFT_MONGODB_DB_URL){
-  mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
+var mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
+
+if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+      mongoUser = process.env[mongoServiceName + '_USER'];
+
+  if (mongoHost && mongoPort && mongoDatabase) {
+    mongoURLLabel = mongoURL = 'mongodb://';
+    if (mongoUser && mongoPassword) {
+      mongoURL += mongoUser + ':' + mongoPassword + '@';
+    }
+    // Provide UI label that excludes user id and pw
+    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+  }
 }
 
 mongoose.Promise = global.Promise;
-mongoose.connect(mongodb_connection_string);
+mongoose.connect(mongoURL);
 
 mongoose.connection.on('connected', function () {
   console.log('Mongoose default connection open');
