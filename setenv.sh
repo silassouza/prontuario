ce229fce5b2cbf9359398fb53ce36996380c47e4
Silas <silasq@gmail.com>
2018-01-16 19:15:06 -0300

    ...

---
+++ setenv.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export MONGODB_USER=prontuario
+export MONGODB_DATABASE=prontuario
+export MONGODB_PASSWORD=prontuario
+export APP_SERVICE_HOST=localhost
+export APP_SERVICE_PORT=27017
